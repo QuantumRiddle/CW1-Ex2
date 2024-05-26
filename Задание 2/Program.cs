1e30ea2a43a67f5ff24b988d2adc6bc5b4f929cc
QuantumRiddle <samurai-1-02@mail.ru>
2024-05-26 05:19:00 +0300

    Добавьте файлы проекта.

---
+++ Задание 2/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Zadanie2
+{
+
+    class Program
+        {
+            static void Main()
+            {
+                int[] myArray = { 2, 5, 7, 12, 8 };
+
+                int[] sortedArray = QuickSort(myArray, 0, myArray.Length - 1);
+
+                Console.WriteLine($"Отсортированный массив: {string.Join(", ", sortedArray)}");
+
+        }
+
+            private static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+            {
+                if (minIndex >= maxIndex)
+                {
+                    return array;
+                }
+
+                int pivotIndex = GetPivotIndex(array, minIndex, maxIndex);
+
+                QuickSort(array, minIndex, pivotIndex - 1);
+
+                QuickSort(array, pivotIndex + 1, maxIndex);
+
+                return array;
+            }
+
+            private static int GetPivotIndex(int[] array, int minIndex, int maxIndex)
+            {
+                int pivot = minIndex - 1;
+
+                for (int i = minIndex; i <= maxIndex; i++)
+                {
+                    if (array[i] < array[maxIndex])
+                    {
+                        pivot++;
+                        Swap(ref array[pivot], ref array[i]);
+                    }
+                }
+
+                pivot++;
+                Swap(ref array[pivot], ref array[maxIndex]);
+
+                return pivot;
+            }
+
+            private static void Swap(ref int leftItem, ref int rightItem)
+            {
+                int temp = leftItem;
+
+                leftItem = rightItem;
+
+                rightItem = temp;
+            }
+        }
+    }
